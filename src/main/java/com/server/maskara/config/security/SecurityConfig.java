fe7169556e83a15edefe8a9fdfb9177667ec4c61
minseong <kdsvip5@naver.com>
2022-08-26 00:15:38 +0900

    Bug(#10) : CORS Error 해결
    - CorsConfigurationSource setAllowedOrigins 수정

--- src/main/java/com/server/maskara/config/security/SecurityConfig.java
+++ src/main/java/com/server/maskara/config/security/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
