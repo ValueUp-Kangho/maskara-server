f23b680ad7e3c998c2a6621b83937aad282721c9
minseong <kdsvip5@naver.com>
2022-08-25 23:28:16 +0900

    Bug(#10) : CORS Error 해결
    - CORS Error 해결을 위한 addCorsMappings 적용

--- src/main/java/com/server/maskara/MascaraApplication.java
+++ src/main/java/com/server/maskara/MascaraApplication.java
@@ -6,9 +6,17 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class MascaraApplication {
+public class MascaraApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MascaraApplication.class, args);
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("*")
+				.allowedMethods("*")
+				.allowedHeaders("*");
 	}
 }
