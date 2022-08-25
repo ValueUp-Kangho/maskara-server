94e7ea5b965a9e643004432f35270288f2c7e5a6
minseong <kdsvip5@naver.com>
2022-08-26 00:12:01 +0900

    Bug(#10) : CORS Error 해결
    - CORS Error 해결을 위한 CorsConfigurationSource 추가
    - 나머지 다 지우고

--- src/main/java/com/server/maskara/MascaraApplication.java
+++ src/main/java/com/server/maskara/MascaraApplication.java
@@ -7,19 +7,9 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class MascaraApplication implements WebMvcConfigurer {
+public class MascaraApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MascaraApplication.class, args);
-	}
-
-
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-				.exposedHeaders("*")
-				.allowedOrigins("*")
-				.allowedHeaders("*")
-				.allowedMethods("*");
 	}
 }
--- src/main/java/com/server/maskara/config/security/SecurityConfig.java
+++ src/main/java/com/server/maskara/config/security/SecurityConfig.java
@@ -17,6 +17,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
+
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -49,5 +51,15 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
