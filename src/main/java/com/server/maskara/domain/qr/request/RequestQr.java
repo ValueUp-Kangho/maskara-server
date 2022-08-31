8468e0e919b33bb110e80c09eccd3bc58e926bd3
uno <svs101@dgu.ac.kr>
2022-09-01 00:00:49 +0900

    Update RequestQr.java

--- src/main/java/com/server/maskara/domain/qr/request/RequestQr.java
+++ src/main/java/com/server/maskara/domain/qr/request/RequestQr.java
@@ -2,7 +2,7 @@ package com.server.maskara.domain.qr.request;
 
 import lombok.Data;
 
-@Data
+@Getter
 public class RequestQr {
     private String collectionBoxSerialNumber;
     private int maskCount;
